b191aabe3298e36708ac45da2f7a2ba7222f493e
Your Name <you@example.com>
2024-01-26 09:35:35 -0800

    Complete

---
+++ src/Lecture06.java
@@ -0,0 +1,74 @@
+class Lecture06 {
+    public static void main(String[] args) {
+        System.out.println("Hello from lecture 06");
+
+        // SELF-CHECK PROBLEMS:
+        // Note: you may need to put some of your answers inside comments
+
+        // Put your answer for #1 here:
+        // Recursion is when A method calls itself to solve smaller portions of a big problem except for the base cases
+        // which it solves directly. It differs from the standard iterative method by memory, the recursive method
+        // adds a new layer to the stack with each call while the iterative method uses a fixed amount.
+        // This causes them to be advantageous in different circumstances.
+
+        // Put your answer for #2 here:
+        // A base case is the simplest form of the problem that can be solved directly without recursion.
+        // A recursive case is when the method calls itself but with a simpler or smaller version of the problem.
+        // The base case prevents infinite loops by providing an end point.
+        // The recursive case solves the problem until you reach the base case.
+
+        // Put your answer for #3 here:
+        // Part A:
+        // 1
+
+        // Part B:
+        // 1, 2
+
+        // Part C:
+        // 1, 3
+
+        // Part D:
+        // 1, 2, 4
+
+        // Part E:
+        // 1, 2, 4, 8, 16
+
+        // Part F:
+        // 1, 3, 7, 15, 30
+
+        // Part G:
+        // 1, 3, 6, 12, 25, 50, 100
+
+        // Put your answer for #5 here:
+        // Part A:
+        // *
+
+        // Part B:
+        // [*]
+
+        // Part C:
+        // ([*])
+
+        // Part D:
+        // ([([*])])
+
+        // Part E:
+        // [([([*])])]
+
+        // Put your answer for #6 here:
+        // Part A:
+        // 4
+
+        // Part B:
+        // 8, 4, 8
+
+        // Part C:
+        // 16, 8, 16
+
+        // Part D:
+        // 12, 8, 4, 8, 12
+
+        // Part E:
+        // 12, 9, 6, 3, 6, 9, 12
+    }
+}
